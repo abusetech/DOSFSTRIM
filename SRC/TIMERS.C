4e417c5d1dd66b636f59fcc712c23d79efdf4d4f
abusetech <60757861+abusetech@users.noreply.github.com>
2021-03-10 13:11:25 -0500

    Added code to loop until a bit in an I/O port register is set or until a timer expires

--- SRC/TIMERS.C
+++ SRC/TIMERS.C
@@ -5,9 +5,9 @@ uint32_t timers_get_tick_count(){
     return __timer_tick_count;
 }
 
-void timers_delay_mills(uint32_t delay){
+void timers_delay_millis(uint32_t delay){
     uint32_t start = __timer_tick_count;
-    while((__timer_tick_count - start) / 55 < delay){}
+    while(((__timer_tick_count - start) /  TIMERS_MILLISECONDS_PER_TICK) < delay){}
 }
 
 interrupt _timer_tick_int_handler(){
@@ -30,5 +30,26 @@ void timers_unhook_timers(){
     enable();
     __timer_tick_count = 0;
     __original_1C_vect = 0;
+}
+
+uint8_t wait_until_IO_bit_set_timeout(uint16_t addr, uint8_t mask, uint32_t timout_ms){
+    /*
+     * Loops until the bit(s) specified by mask are set in the 8 bit I/O port at addr
+     * or until the timeout_ms milliseconds have elapsed.
+     * 
+     * If the timer expires, this function returns zero
+     * If one or more bits are set, this function returns a value indicating which 
+     * bits are set specifically, (mask & <io port value>)
+     * 
+     */
+    uint8_t prt = 0;
+    uint32_t start = 0;
+    while(((__timer_tick_count - start) /  TIMERS_MILLISECONDS_PER_TICK) < timeout_ms){
+        prt = inportb(addr) & mask;
+        if (prt){
+            return prt;
+        }
+    }
+    return 0;
 }
 
