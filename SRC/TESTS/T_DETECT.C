578e27c1f994efff28f18dee1991425fb68c5db3
abusetech <60757861+abusetech@users.noreply.github.com>
2021-03-12 11:55:27 -0500

    Updated drive detection program to properly select the device on the bus before an IDENTIFY_DEVICE command

--- SRC/TESTS/T_DETECT.C
+++ SRC/TESTS/T_DETECT.C
@@ -23,8 +23,10 @@ main(){
     printf("IDE Primary bus status %#04X\n", ATA_read_status_reg(ATA_PRIMARY_IO_BASE));
      if(!(ATA_read_status_reg(ATA_PRIMARY_IO_BASE) == 0xFFFF)){
          /* Check if *any* devices are on the bus */
+          ATA_select_drive(ATA_PRIMARY_IO_BASE, ATA_IDE_DEVICE_0);
           summary = ATA_identify_summary(ATA_PRIMARY_IO_BASE, ATA_IDE_DEVICE_0, ATA_TIMEOUT);
           pretty_print(&summary);
+          ATA_select_drive(ATA_PRIMARY_IO_BASE, ATA_IDE_DEVICE_1);
           summary = ATA_identify_summary(ATA_PRIMARY_IO_BASE, ATA_IDE_DEVICE_1, ATA_TIMEOUT);
           pretty_print(&summary);
      }
@@ -32,8 +34,10 @@ main(){
      printf("IDE secondary bus status %#04X\n", ATA_read_status_reg(ATA_SECONDARY_IO_BASE));
       if(!(ATA_read_status_reg(ATA_SECONDARY_IO_BASE) == 0xFFFF)){
          /* Check if *any* devices are on the bus */
+          ATA_select_drive(ATA_SECONDARY_IO_BASE, ATA_IDE_DEVICE_0);
           summary = ATA_identify_summary(ATA_SECONDARY_IO_BASE, ATA_IDE_DEVICE_0, ATA_TIMEOUT);
           pretty_print(&summary);
+          ATA_select_drive(ATA_SECONDARY_IO_BASE, ATA_IDE_DEVICE_1);
           summary = ATA_identify_summary(ATA_SECONDARY_IO_BASE, ATA_IDE_DEVICE_1, ATA_TIMEOUT);
           pretty_print(&summary);
      }
